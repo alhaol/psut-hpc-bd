f5872f53a1b8d6f4ee65e1aaf8b4e30d96371077
alhaol <ibrahimee@ieee.org>
2021-04-12 12:11:57 -0400

    Updated Mongo

---
+++ IntroToPython/examples/ch17/TwitterMongoDB/.ipynb_checkpoints/keys-checkpoint.py
@@ -0,0 +1,8 @@
+consumer_key = 'i2thFb9OwWr85PrUmvGb2eLlw'
+consumer_secret = 'tFsFAof50RzqryjI9paUnpLl7DneaZnbrZwwOUo09st1fBTcl7'
+access_token = '538749818-lQqdndyigZ2jVbnXtGJbHGGMCKgfkWCxe7qQ0Yp9'
+access_token_secret = 'pCXKY5V8UAHDFztRHeh8qkvaq9ZqmG2oy3Ua38AKKhr3q'
+
+mapquest_key = 'DyeUHfGAaWWphBUFLIlqwlEmbtZEd8Fq'
+
+mongo_connection_string = 'mongodb+srv://cluster0.p4czi.mongodb.net/myFirstDatabase'
